966264680f997127a9392adeca2d2fd154a8264b
Daniel Grijuela <danielgrijuela@gmail.com>
2015-04-14 22:57:59 +0200

    Improve roulette

--- src/naktis/core.clj
+++ src/naktis/core.clj
@@ -6,12 +6,12 @@
 ;; Helper to print status' properties
 (defn print-status
   []
-  (println (str "Eres un " (:stereotype @status)))
-  (println (str "Dinero: " (:money @status) " euros"))
-  (println (str "Sex-appeal: " (:sex-appeal @status) "/10"))
-  (println (str "Labia: " (:mouth @status) "/10"))
-  (println (str "Alcohol en vena: " (:alcohol @status)))
-  (println (str "La hora es: " (format "%.2f" (:hour @status)))))
+  (print (str "| Tribu: " (:stereotype @status) " | "))
+  (print (str "Dinero: " (:money @status) " € | "))
+  (print (str "Sex-appeal: " (:sex-appeal @status) "/10 | "))
+  (print (str "Labia: " (:mouth @status) "/10 | "))
+  (print (str "Alcohol: " (:alcohol @status) " | "))
+  (println (str "Hora: " (format "%.2f" (:hour @status)) " |")))
 
 (defn play
   [])
@@ -25,7 +25,7 @@
   (int (+ lower-limit (* (rand) upper-limit))))
 
 ;; Randomize initial state
-(defn ruleta
+(defn roulette
   []
   (reset! status {:alcohol    (format "%.2f" (rand 0.1)) ; Just two decimals
                   :money      (randomize 5 100)
@@ -33,7 +33,7 @@
                   :mouth      (randomize 2 10)
                   :hour       22.0
                   :stereotype (name (rand-nth stereotypes))})
-  (println "Te ha tocado:")
+  (println "Esto es lo que te ha tocado:")
   (print-status)
   (play))
 
@@ -41,7 +41,25 @@
 (defn -main
   []
   (println "Comienza el juego, comienza tu naktis...")
-  (println "Una naktis dura desde las 22:00 horas hasta las 06:00 del día siguiente")
-  (println "Sólo saldrás vencedor de ella si consigues mojar con una mujer humana")
-  (println "Pero antes que nada, tiremos de la ruleta de la vida...")
-  (ruleta))
+  (Thread/sleep 3000)
+  (println "Una naktis es una noche de la que sólo saldrás vencedor si consigues mojar con una mujer humana.")
+  (Thread/sleep 3000)
+  (println "Tienes desde las 22:00 horas que son ahora hasta las 06:00 de mañana para lograr tu objetivo.")
+  (Thread/sleep 3000)
+  (println)
+  (println "Pero antes que nada, tira de la ruleta de la vida...")
+  (println "  1")
+  (println "4   2")
+  (println "  3")
+  (println "Elige un numero desde el que girar.")
+  (do (print ">> ")
+      (flush)
+      (let [input (read-line)]
+        (when (and (not= input "1") (not= input "2") (not= input "3") (not= input "4"))
+          (println "Empiezas mal la noche... Voy a tirar desde el 1"))))
+  (Thread/sleep 3000)
+  (println "...")
+  (Thread/sleep 3000)
+  (println "...")
+  (Thread/sleep 3000)
+  (roulette))
