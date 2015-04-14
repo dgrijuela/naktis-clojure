0283f8467d313dacd9bead5051020e4a82aab286
Daniel Grijuela <danielgrijuela@gmail.com>
2015-04-14 21:44:19 +0200

    Make the status an atom and fix some things to make it work

--- src/naktis/core.clj
+++ src/naktis/core.clj
@@ -1,31 +1,38 @@
 (ns naktis.core)
 
+;; This is the global status of the game
+(def status (atom nil))
+
+;; Helper to print status' properties
 (defn print-status
-  [status]
-  (println (str "Eres un " (name (:stereotype status))))
-  (println (str "Money: " (:money status) " euros"))
-  (println (str "Sex-appeal: " (:sex-appeal status) "/10"))
-  (println (str "Mouth power: " (:mouth status) "/10"))
-  (println (str "Alcohol in vein: " (:alcohol status)))
-  (println (str "The hour is: " (format "%.2f" (:hour status)))))
+  []
+  (println (str "Eres un " (:stereotype @status)))
+  (println (str "Dinero: " (:money @status) " euros"))
+  (println (str "Sex-appeal: " (:sex-appeal @status) "/10"))
+  (println (str "Labia: " (:mouth @status) "/10"))
+  (println (str "Alcohol en vena: " (:alcohol @status)))
+  (println (str "La hora es: " (format "%.2f" (:hour @status)))))
 
 (defn play
-  [status])
+  [])
 
-(def stereotypes [:pijo, :cani, :heavy, :emo, :hipster, :rapero])
+;; Let's keep it simple for now (:heavy, :emo, :hipster, :rapero...)
+(def stereotypes [:pijo, :cani])
 
+;; Randomize initial state
 (defn ruleta
   []
-  (let [status {:alcohol    0
-                :money      (rand-int 100)
-                :sex-appeal (rand-int 10)
-                :mouth      (rand-int 10)
-                :hour       22.00
-                :stereotype (rand-nth stereotypes)}]
-    (println "Te ha tocado:")
-    (print-status status)
-    (play status)))
+  (reset! status {:alcohol    0
+                  :money      (rand-int 100)
+                  :sex-appeal (rand-int 10)
+                  :mouth      (rand-int 10)
+                  :hour       22.00
+                  :stereotype (name (rand-nth stereotypes))})
+  (println "Te ha tocado:")
+  (print-status)
+  (play))
 
+;; The game begins...
 (defn -main
   []
   (println "Comienza el juego, comienza tu naktis...")
